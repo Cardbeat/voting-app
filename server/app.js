e4d785d4e8d26d7dcd75c45d0d4356553766a5d8
jefferson oliveira <cardbeatt@gmail.com>
2017-03-22 17:02:55 -0300

    first commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app= express();
+
+app.get('*', (req , res ) => {
+  res.send('funcionou!');
+});
+
+const port = process.env.NODE_ENV || 3000;
+
+app.listen(port, () =>  { console.log(`running app at ${port}`) });
