7bd3df5f1d5e361b383d82942e19478ec693a6aa
jefferson oliveira <cardbeatt@gmail.com>
2017-04-12 14:16:12 -0300

    started the process to create polls

--- public/scripts/components/NewPoll.js
+++ public/scripts/components/NewPoll.js
@@ -1,10 +1,44 @@
 import React from 'react';
 
 export default class NewPoll extends React.Component {
+
+
+
   render() {
+    function AddOption() {
+      return (
+        <div className='row'>
+          <div className='input-field col s6  offset-s3'>
+            <input id='choices' className='validade' type='text' name='choices'/>
+            <label htmlFor="choices">Option</label>
+          </div>
+          <a onClick=''>Add</a>
+        </div>
+      )
+    }
+
     return (
-      <div>
-        <h1> form for new poll</h1>
+      <div className='container'>
+        <form className='col s12' action='#' method='post'>
+          <div className='row'>
+            <div className='input-field col s6  offset-s3'>
+              <input id='question' className='validade' type='text' name='question'/>
+              <label htmlFor="question">Question</label>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='input-field col s6  offset-s3'>
+              <input id='choices' className='validade' type='text' name='choices'/>
+              <label htmlFor="choices">Option</label>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='input-field col s6  offset-s3'>
+              <input id='choices' className='validade' type='text' name='choices'/>
+              <label htmlFor="choices">Option</label>
+            </div>
+          </div>
+        </form>
       </div>
     )
   }
